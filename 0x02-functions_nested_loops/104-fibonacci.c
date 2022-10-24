db0e4972e21fe38ace92d026e20200a9a611c2d3
David <livingst76@yahoo.com>
2022-10-24 10:22:53 +0100

    fibonacci numbers3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,26 +7,44 @@
  */
 int main(void)
 {
-	unsigned long fibonacci[3];
-	int i;
-
-	fibonacci[0] = 0;
-	fibonacci[1] = 1;
+	unsigned long fib1, n1 = 0, n2 = 1;
+	int i, fib2, n3, n4;
 
 	for (i = 2; i < 100; i++)
 	{
-		fibonacci[2] = fibonacci[1] + fibonacci[0];
-		if (i < 99)
+		if (i < 93)
 		{
-			printf("%lu, ", fibonacci[2]);
+			fib1 = n2 + n1;
+			n1 = n2;
+			n2 = fib1;
+			printf("%lu, ", fib1);
 		}
 		else
 		{
-			printf("%lu\n", fibonacci[2]);
+			if (i == 93)
+			{
+				n3 = n1 / 1000000000000000000;
+				n4 = n2 / 1000000000000000000;
+			}
+			n1 = n1 % 1000000000000000000;
+			n2 = n2 % 1000000000000000000;
+			fib1 = n1 + n2;
+			fib2 = n3 + n4 + (fib1 / 1000000000000000000);
+			n1 = n2;
+			n2 = fib1 % 1000000000000000000;
+			n3 = n4;
+			n4 = fib2;
+			if (i < 99)
+			{
+				printf("%i", fib2);
+				printf("%lu, ", fib1 % 1000000000000000000);
+			}
+			else
+			{
+				printf("%i", fib2);
+				printf("%lu\n", fib1 % 1000000000000000000);
+			}
 		}
-
-		fibonacci[0] = fibonacci[1];
-		fibonacci[1] = fibonacci[2];
 	}
 	return (0);
 }
