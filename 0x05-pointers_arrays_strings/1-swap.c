74946f412ef502d7398eab2cee2304ab13db6b2a
David <livingst76@yahoo.com>
2022-10-29 10:43:08 +0100

    fixed linter error

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 /**
  * swap_int - swap the value of two integers
  *
- * @*a: pointer to first integer
- * @*b: pointer to second integer
+ * @a: pointer to first integer
+ * @b: pointer to second integer
  */
 void swap_int(int *a, int *b)
 {
