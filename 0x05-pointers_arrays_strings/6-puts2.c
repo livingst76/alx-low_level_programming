6dc098dca2b6e30563dd3ad12d770757ed149e27
David <livingst76@yahoo.com>
2022-10-30 20:12:22 +0100

    checked for empty string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,16 @@ void puts2(char *str)
 {
 	char *p = str;
 
-	do {
-		_putchar(*p);
-		p += 2;
-	} while (*p != '\0' && *(p - 1) != '\0');
+	if (*p == '\0')
+	{
+		_putchar('\0');
+	}
+	else
+	{
+		do {
+			_putchar(*p);
+			p += 2;
+		} while (*p != '\0' && *(p - 1) != '\0');
+	}
 	_putchar('\n');
 }
