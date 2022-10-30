0c6e4461a8c3cd3ae09f041303549e37dc5785ad
David <livingst76@yahoo.com>
2022-10-30 18:04:32 +0100

    implemented function, rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - print inputed string as output in reverse order
+ *
+ * @s: inputed string
+ */
+void rev_string(char *s)
+{
+	char tmp, *p = s;
+	int i = 0, n, len = 0;
+
+	while (*p != '\0')
+	{
+		len++;
+		p++;
+	}
+	n = len + 1;
+
+	while (n > 1)
+	{
+		tmp = *p;
+		*p = *(p - n);
+		*(p - n) = tmp;
+		i++;
+		p--;
+		n = n - 2;
+	}
+}
