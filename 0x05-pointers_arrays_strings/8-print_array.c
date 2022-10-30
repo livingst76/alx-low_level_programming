4dfe8bee06ec1ba15f71f1754dbdd9fda9ef2310
David <livingst76@yahoo.com>
2022-10-30 21:16:50 +0100

    implemeted function, print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elemnets in an array
+ *
+ * @n - number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n- 1)
+		{
+			printf("%i, ", *(a + i));
+		}
+		else
+		{
+			printf("%i\n", *(a + i));
+		}
+	}
+}
