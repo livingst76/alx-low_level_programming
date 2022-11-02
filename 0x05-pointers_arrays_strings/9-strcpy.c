625eff7a8ccd9687e7540e92bdef939686ab6707
David <livingst76@yahoo.com>
2022-11-02 08:21:51 +0100

    implemented function, _strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string
+ *
+ * @src: pointer to the string to be copied
+ * @drc: string to be copied to
+ *
+ * Return: the pointer to dest;
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *cp;
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+	cp = dest;
+    return (cp);
+}
