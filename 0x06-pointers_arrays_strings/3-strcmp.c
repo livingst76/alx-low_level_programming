937f8d385ca5c6e2f5cb7e75a1e160169a2eafc0
David <livingst76@yahoo.com>
2022-11-03 11:21:15 +0100

    implemented function, _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string to be compared with/to
+ * @s2: string to be compared
+ *
+ * Return: boolean: 0 if they are equal; negative value if s1 is less than s2
+ *         else, a  postive value
+ */
+ int _strcmp(char *s1, char *s2)
+ {
+	 int boolean = 0, diff = 0, len1 = 0, len2 = 0;
+
+	 while (s1[len1] != '\0' || s2[len2] != '\0')
+	 {
+		 if (s1[len1] != '\0')
+		 {
+			 len1++;
+		 }
+
+		 if (s2[len2] != '\0')
+		 {
+			 len2++;
+		 }
+
+		 if (s1[len1] != s2[len2])
+		 {
+			 diff++;
+		 }
+	 }
+
+	 if (diff > 0 || len1 != len2)
+	 {
+		 boolean = (int) s1[len1 - 1] - (int) s2[len2 - 2];
+	 }
+
+	 return (boolean);
+ }
