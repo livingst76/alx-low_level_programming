4b7761fc9d0da84add49caa0a53b173b41baa715
David <livingst76@yahoo.com>
2022-11-03 12:19:59 +0100

    modified algorithm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,38 +12,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int boolean = 0, diff = 0, len1 = 0, len2 = 0;
+	int diff = 0, len = 0;
 
-	while (s1[len1] != '\0' || s2[len2] != '\0')
+	while (diff == 0)
 	{
-		if (s1[len1] != '\0')
+		if  (s1[len] == '\0' && s2[len] == '\0')
 		{
-			len1++;
+			break;
 		}
-
-		if (s2[len2] != '\0')
-		{
-			len2++;
-		}
-
-		if (s1[len1] != s2[len2])
-		{
-			diff++;
-		}
+		diff = s1[len] - s2[len];
+		len++;
 	}
 
-	if (diff > 0 && len1 > len2)
-	{
-		boolean = s1[len1 - 1] - 48;
-	}
-	else if (diff > 0 && len1 < len2)
-	{
-		boolean = 48 - s2[len2 - 1];
-	}
-	else if (diff > 0 && len1 == len2)
-	{
-		boolean = s1[len1 - 1] - s2[len2 - 1];
-	}
-
-	return (boolean);
+	return (diff);
 }
