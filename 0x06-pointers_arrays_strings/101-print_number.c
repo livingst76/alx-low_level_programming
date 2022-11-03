9a05abd315e72e628cbe328d86e109924188b775
David <livingst76@yahoo.com>
2022-11-03 19:23:25 +0100

    implemented function, print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_number - displays input (number) as output
+ *
+ * @n: integer/number
+ *
+ * Return: returns no value
+ */
+void print_number(int n)
+{
+	int i, p, y;
+	unsigned int m, k;
+
+	y = 0;
+	p = 1;
+	k = m = n;
+
+	if (n < 0)
+	{
+		k = m = n * -1;
+		_putchar('-');
+	}
+
+	while (m > 9)
+	{
+		m /= 10;
+		y++;
+	}
+
+	for (i = 0; i < y; i++)
+	{
+		p *= 10;
+	}
+
+	for (i = 0; i <= y; i++)
+	{
+		_putchar(((k / p) % 10) + '0');
+		p /= 10;
+	}
+}
