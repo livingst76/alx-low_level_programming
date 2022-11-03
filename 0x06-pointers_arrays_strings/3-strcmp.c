4c598ca7cdb74633074b8a99d42da7de6cc3df33
David <livingst76@yahoo.com>
2022-11-03 11:27:54 +0100

    implemented function, _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,32 +9,36 @@
  * Return: boolean: 0 if they are equal; negative value if s1 is less than s2
  *         else, a  postive value
  */
- int _strcmp(char *s1, char *s2)
- {
-	 int boolean = 0, diff = 0, len1 = 0, len2 = 0;
+int _strcmp(char *s1, char *s2)
+{
+	int boolean = 0, diff = 0, len1 = 0, len2 = 0;
 
-	 while (s1[len1] != '\0' || s2[len2] != '\0')
-	 {
-		 if (s1[len1] != '\0')
-		 {
-			 len1++;
-		 }
+	while (s1[len1] != '\0' || s2[len2] != '\0')
+	{
+		if (s1[len1] != '\0')
+		{
+			len1++;
+		}
 
-		 if (s2[len2] != '\0')
-		 {
-			 len2++;
-		 }
+		if (s2[len2] != '\0')
+		{
+			len2++;
+		}
 
-		 if (s1[len1] != s2[len2])
-		 {
-			 diff++;
-		 }
-	 }
+		if (s1[len1] != s2[len2])
+		{
+			diff++;
+		}
+	}
 
-	 if (diff > 0 || len1 != len2)
-	 {
-		 boolean = (int) s1[len1 - 1] - (int) s2[len2 - 2];
-	 }
+	if (diff > 0 && len1 > len2)
+	{
+		boolean = 15;
+	}
+	else if (diff > 0 && len1 < len2)
+	{
+		boolean = -15;
+	}
 
-	 return (boolean);
- }
+	return (boolean);
+}
