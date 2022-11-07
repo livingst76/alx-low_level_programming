f5c147c9d4d42beb3d7b3aabb63bfe2d60986dac
David <livingst76@yahoo.com>
2022-11-07 14:59:04 +0100

    fixed check 2

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,7 +15,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, len1 = 0, len2 = 0, n;
+	int carry = 0, i, len1 = 0, len2 = 0, n;
 
 	while (*(n1 + len1) != '\0'  || *(n2 + len2) != '\0')
 	{
@@ -26,18 +26,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	if (len1 >= size_r  || len2 >= size_r || len1 == size_r - 1)
 		return (r = NULL);
-	if (len1 > len2)
-		n = len1;
-	else
-		n = len2;
-	for (r[n + 1] = '\0' ; len1 >= 1 ||  len2 >= 1; n--)
+	for (n = len1, r[n + 1] = '\0' ; len1 >= 1 ||  len2 >= 1; n--)
 	{
-		if (len1 < 1)
-		{
-			r[n] = 48 + (((carry + n2[len2 - 1]) - 48) % 10);
-			carry = ((carry + n2[len2 - 1]) - 48) / 10;
-		}
-		else if (len2 < 1)
+		if (len2 < 1)
 		{
 			r[n] = 48 + (((carry + n1[len1 - 1]) - 48) % 10);
 			carry = ((carry + n1[len1 - 1]) - 48) / 10;
@@ -54,5 +45,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	if (carry > 0)
 		r[0] = '1';
+	else
+	{
+		for (i = 0; r[i + 1] != '\0'; i++)
+			r[i] = r[i + 1];
+		r[i] = '\0';
+	}
 	return (r);
 }
