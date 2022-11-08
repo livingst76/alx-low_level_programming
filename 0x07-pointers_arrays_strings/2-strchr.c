2e96f59ee108a9b3fd3b5c9438b872eb44059071
David <livingst76@yahoo.com>
2022-11-08 10:48:06 +0100

    implemented function, _strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - find the first occurence of a character in a string
+ *
+ * @s: string
+ * @c: character to be found
+ *
+ * Return: p, pointer to the first occurence of c; NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int boolean = 0, i = 0;
+    char  **p = &s;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			*p = &s[i];
+			boolean = 1;
+			break;
+		}
+		i++;
+	}
+
+	if (boolean)
+		return (*p);
+	else
+		return (*p = NULL);
+}
