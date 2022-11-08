e5a02e662ea07e96746dd57c15371c5649676eef
David <livingst76@yahoo.com>
2022-11-08 11:33:12 +0100

    fixed check 5

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - find the first occurence of a character in a string
@@ -10,17 +11,20 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	int boolean = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (&s[i]);
+			boolean = 1;
+			break;
 		}
-		i++;
 	}
 
-	return ('\0');
-
+	if (boolean)
+		return (&s[i]);
+	else
+		return (NULL);
 }
