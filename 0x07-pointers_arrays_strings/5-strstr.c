1a23e0e652ca13cde40344c15319a9a3b068f3ef
David <livingst76@yahoo.com>
2022-11-08 15:27:58 +0100

    implemented function, _strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * _strstr - find the first occurence of a substring in a string
+ *
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: &s[i], address of first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int c = 0, i, j, m = 0, n;
+	int boolean = 0;
+
+	while (needle[m] != '\0')
+	{
+		m++;
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0, n = i; j < m; j++, n++)
+			{
+				if (haystack[n] == needle[j])
+					c++;
+				else
+				{
+					c = 0;
+					break;
+				}
+			}
+			if (c == m)
+			{
+				printf("c: %u\nm: %u\n", c, m);
+				boolean = 1;
+				break;
+			}
+		}
+		if (boolean)
+			break;
+	}
+	if (boolean)
+		return (&haystack[i]);
+	else
+		return (NULL);
+}
