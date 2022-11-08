23b59e8bfc432381035b1f26e9b094202d96364e
David <livingst76@yahoo.com>
2022-11-08 11:24:39 +0100

    fixed check 5

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,22 +11,11 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
-	int boolean = 0;
-	char *p = s;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			p = &s[i];
-			boolean = 1;
-			break;
-		}
-		i++;
-	}
-
-	if (boolean)
-		return (p);
-	else
-		return ('\0');
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
