4eb44c1ca31595d681b0d7a2150390971ca45fd7
David <livingst76@yahoo.com>
2022-11-09 13:11:59 +0100

    fixed check 5, check 4, and check 3

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -26,8 +26,8 @@ void print_diagsums(int *a, int size)
 		}
 		n = j - (size * 2);
 		if (!i)
-			printf("%lu,  ", sum);
+			printf("%lu", sum);
 		else
-			printf("%lu\n", sum);
+			printf(", %lu\n", sum);
 	}
 }
