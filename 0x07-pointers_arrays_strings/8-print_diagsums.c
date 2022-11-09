38a2394d65220d9f12385a6e85ad7614f2726509
David <livingst76@yahoo.com>
2022-11-09 13:41:41 +0100

    fixed check 5 amd check 4

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	int i;
 	unsigned long sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size * size; i++)
+	for (i = 0; i < (size * size); i++)
 	{
 		if (!(i % (size + 1)))
 			sum1 += a[i];
