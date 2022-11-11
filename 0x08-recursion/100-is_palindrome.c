e013ae5ee5178fa8b6f8632b2aa7a899e4cbf518
David <livingst76@yahoo.com>
2022-11-11 15:39:12 +0100

    fixed check 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+void palindromity(char *s, int i, int j, int *boolean);
+int _strlen_recursion(char *s);
+
 /**
  * is_palindrome - check if the string is empty(a palindrome)
  *
@@ -9,8 +12,55 @@
  */
 int is_palindrome(char *s)
 {
+	int len, boolean = 0;
+
+	if (!*s)
+		return (1);
+
+	len = _strlen_recursion(s);
+	palindromity(s, len - 1, len, &boolean);
+	if (boolean)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * palindromity - test string for palindromity
+ *
+ * @s: string
+ * @i: string index
+ * @j: length of string
+ * @b: pointer to boolean
+ */
+void palindromity(char *s, int i, int j, int *b)
+{
+	if (i < 0)
+		return;
+	if (*s == s[i])
+	{
+		*b = 1;
+	}
+	else if (*s != s[i])
+	{
+		*b = 0;
+		return;
+	}
+
+	palindromity(s + 1, i - 2, j, b);
+}
+
+/**
+ * _strlen_recursion - calculate the length of a string;
+ *
+ * @s: string
+ *
+ * Return: len, length of string
+ */
+int _strlen_recursion(char *s)
+{
 	if (*s == '\0')
 		return (0);
 	else
-		return (1);
+		return (1 + _strlen_recursion(s + 1));
 }
