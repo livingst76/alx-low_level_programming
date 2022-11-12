c0456c474744f69850efd2e5a5830099c9b70c52
David <livingst76@yahoo.com>
2022-11-12 08:56:02 +0100

    we begin!

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ *
+ * @argc: number of command line arguments
+ * @argv: pointer to string of command line arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
