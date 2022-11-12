96c2cf94996ccb37fe357b4e9ab4b7a1dc7d785b
David <livingst76@yahoo.com>
2022-11-12 12:16:17 +0100

    cool addition style! :)

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ *
+ * @argc: number of command line arguments
+ * @argv: pointer to string of command line arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(__attribute__ ((unused)) int argc, char *argv[])
+{
+	int i, j, m, result = 0;
+
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < 3; i++)
+	{
+		m = 0;
+		for (j = 0; argv[i][j] != '\0'; j++)
+			m = (m * 10) + (argv[i][j] - 48);
+		result = result + m;
+	}
+
+	printf("%i\n", result);
+	return (0);
+}
