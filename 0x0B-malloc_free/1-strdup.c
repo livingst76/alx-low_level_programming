cfce1ebf12b89ae494f575013cfd01a100c1853b
David <livingst76@yahoo.com>
2022-11-13 19:31:38 +0100

    implemented function, _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate string given as a parameter.
+ *
+ * @str: string.
+ *
+ * Return: pointer to duplicate; NULL if str = NULL or empty.
+ */
+char *_strdup(char *str)
+{
+	char *dp;
+	unsigned int len = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (str[len])
+		len++;
+	dp = malloc(len * sizeof(char));
+	if (!dp)
+		return (NULL);
+	while (len)
+	{
+		len--;
+		dp[len] = str[len];
+	}
+	return (dp);
+}
