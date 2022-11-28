14110a5578f06065e6ae6163aee72422b483d35d
David <livingst76@yahoo.com>
2022-11-28 13:56:12 +0100

    project details; defined the struct, dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - dog's profile
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*MAIN_H*/
