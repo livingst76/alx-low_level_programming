3d1e783f399774544e21565b0306126762a7e511
David <livingst76@yahoo.com>
2022-11-28 19:38:19 +0100

    my first functionn pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name
+ *
+ * @name: pointer to the string of the name to be printed
+ * @f: function to convert name to uppercase and print it
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+
+	f(name);
+}
