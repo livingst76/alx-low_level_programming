fb3adfa299408d3dc0cc95dd5d60dbce9bb59dd8
David <livingst76@yahoo.com>
2022-11-28 15:21:28 +0100

    created a new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * new_dog - create profile details for a new dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: new_dog, pointer to new dog's profile details.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t new_dog, *p = &new_dog;
+	char *nm = name, *ow = owner;
+
+	if (!name)
+		return (NULL);
+
+	if (!owner)
+		return (NULL);
+
+	new_dog.name = nm;
+	new_dog.age = age;
+	new_dog.owner = ow;
+
+	if (!p)
+		return (NULL);
+
+	return (p);
+}
