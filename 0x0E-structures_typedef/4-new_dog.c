4194e3ab195d19fd905a548d81558d95fd5edb33
David <livingst76@yahoo.com>
2022-11-28 15:55:52 +0100

    fixed check 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *nm, *ow;
 	int len = 0, i;
 
-	if (!name || !owner)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 	while (name[len])
 		len++;
