0952da5b5c20ef152a0d96b3248a9e4fcea5c1d1
David <livingst76@yahoo.com>
2022-11-29 20:40:47 +0100

    fixed check 8

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
+		printf("%i", va_arg(ap, int));
+
 		if (separator && i < n - 1)
-			printf("%i%s", va_arg(ap, int), separator);
+			printf("%s", va_arg(ap, int), separator);
 	}
 	printf("\n");
 	va_end(ap);
