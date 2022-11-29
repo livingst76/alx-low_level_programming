ad1640ea7eeac113848294ac1fa43ea046afafe8
David <livingst76@yahoo.com>
2022-11-29 20:25:38 +0100

    my tyepsetter:)

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers given as parameters followed by a new line
+ *
+ * @separator: string to be printed between numbers
+ * @n: number of integers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (!n)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			if (separator)
+				printf("%i%s", va_arg(ap, int), separator);
+			else
+				printf("%i", va_arg(ap, int));
+		}
+		else
+		{
+			printf("%i\n", va_arg(ap, int));
+		}
+	}
+}
