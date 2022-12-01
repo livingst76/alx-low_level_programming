9d5b399c257dd0e4f0604ba392b31c925fa29f24
David <livingst76@yahoo.com>
2022-12-01 23:02:53 +0100

    free and free indeed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free the list
+ *
+ * @head: pointer to the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head->next)
+	{
+		free(head->str);
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
