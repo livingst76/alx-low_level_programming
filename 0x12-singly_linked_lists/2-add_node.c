9d1a74c1fb6768b1d3e913fe89d9773f2e877c0f
David <livingst76@yahoo.com>
2022-12-01 17:11:41 +0100

    fixed check 9

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,11 +26,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	if (!new->str)
-	{
-		free(new->str);
-		return (NULL);
-	}
 	new->len = len;
 	new->next = *head;
 	*head = new;
