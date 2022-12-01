b4abf0d917bf51ea999f9d000b49ed2358e0ac0c
David <livingst76@yahoo.com>
2022-12-01 21:26:40 +0100

    remodification

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,16 +16,23 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	int len = 0;
 
-	while (str[len])
-		len++;
-
 	new = malloc(sizeof(list_t));
 	if (!new)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
+
+	if (str)
+	{
+		new->str = strdup(str);
+		while (str[len])
+			len++;
+	}
+	else
+	{
+		new->str = NULL;
+	}
 	new->len = len;
 	new->next = *head;
 	*head = new;
