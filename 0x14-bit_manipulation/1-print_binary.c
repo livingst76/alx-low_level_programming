e2b57fb56bb8792db61659ff93445d132481b8fe
David <livingst76@yahoo.com>
2022-12-02 13:03:11 +0100

    decinal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,19 @@ void print_binary(unsigned long int n)
 	unsigned int tmp = n;
 	int i, j, k;
 
-	if (n < 0)
+	if (!n)
+	{
+		_putchar('0');
 		return;
+	}
 
 	for (i = 0; tmp > 0; i++, tmp >>= 1)
 		;
 
 	k = j = i;
 
-	do {
+	while (i > 0)
+	{
 		tmp = n;
 		for (j = k; j > 1; j--)
 			n >>= 1;
@@ -27,6 +31,5 @@ void print_binary(unsigned long int n)
 		n = tmp;
 		n <<= 1;
 		i--;
-	} while (i > 0);
-
+	}
 }
