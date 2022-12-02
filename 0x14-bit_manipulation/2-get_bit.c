79e0d585a0731f92711d949eb70b37a574ce4678
David <livingst76@yahoo.com>
2022-12-02 14:41:50 +0100

    binary indexing

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit at the given index of the decimal number
+ *
+ * @n: decimal number
+ * @index: index to be used
+ *
+ * Return: value of the bit, -1 on failure (error).
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int tmp = n;
+	unsigned int i, j;
+
+	for (i = 0; tmp > 0; tmp >>= 1, i++)
+		;
+
+	if (index >= i)
+		return (-1);
+
+	for (j = 0; j < index; j++)
+		n >>= 1;
+	return (n & 1);
+}
