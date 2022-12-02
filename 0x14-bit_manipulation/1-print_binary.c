46b9cace23ef9ca3f6ae78bfa8cdeea6ca573bd0
David <livingst76@yahoo.com>
2022-12-02 12:58:15 +0100

    decinal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * print_binary - print the binary representation of the decimal number
@@ -8,37 +7,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, rem;
 	unsigned int tmp = n;
-	char *bit;
+	int i, j, k;
 
-	if (!n)
-	{
-		_putchar('0');
-		return;
-	}
+	for (i = 0; tmp > 0; i++, tmp >>= 1)
+		;
 
-	while (tmp > 0)
-	{
-		tmp = tmp / 2;
-		i++;
-	}
+	k = j = i;
 
-	bit = malloc((i + 1) * sizeof(char));
-	if (!bit)
-		return;
-	bit[i] = '\0';
-	i--;
-
-	while (n > 0)
-	{
-		rem = n % 2;
-		n = n / 2;
-		bit[i] = 48 + rem;
+	do {
+		tmp = n;
+		for (j = k; j > 1; j--)
+			n >>= 1;
+		_putchar((n & 1) + '0');
+		j = k;
+		n = tmp;
+		n <<= 1;
 		i--;
-	}
+	} while (i > 0);
 
-	for (i = 0; bit[i]; i++)
-		_putchar(bit[i]);
-	free(bit);
 }
