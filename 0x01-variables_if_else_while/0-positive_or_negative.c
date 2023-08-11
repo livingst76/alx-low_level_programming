5de1af2f0549c8620723bf48a19b499e7e5ea285
David Livingstone <livingst76@yahoo.com>
2023-08-11 13:31:30 +0100

    removed braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%i is positive\n", n);
-	}
+
 	if (n == 0)
-	{
 		printf("%i is zero\n", n);
-	}
+
 	if (n < 0)
-	{
 		printf("%i is negative\n", n);
-	}
 
 	return (0);
 }
