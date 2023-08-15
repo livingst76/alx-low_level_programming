979039a2c94d5861b4981468dda2e3fbf897a702
David Livingstone <livingst76@yahoo.com>
2023-08-15 15:33:06 +0100

    fix: sort condition for numbers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if ((a > b && b > c) || (a > c && c > b) || (a > b && b == c))
+	{
+		largest = a;
+	}
+	else if ((b > a && a > c) || (b > c && c > a) || (b > a && a == c))
+	{
+		largest = b;
+	}
+	else if ((c > a && a > b) || (c > b && b > c) || (c > a && a == b))
+	{
+		largest = c;
+	}
+	else if ((a == b && b > c) || (a == c && c > b))
+	{
+		largest = a;
+	}
+	else if ((b == a && a > c) || (b == c && c > a))
+	{
+		largest = b;
+	}
+	else if ((c == b && b > a) || (c == a && a > b))
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = a;
+	}
+
+	return (largest);
+}
