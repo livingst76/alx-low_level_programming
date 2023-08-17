3d50a4153608be6898509c6b20a6abb9f04c08ca
David Livingstone <livingst76@yahoo.com>
2023-08-17 13:07:05 +0100

    Create file for Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check the case of a character
+ *
+ * @c: character
+ *
+ * Return: 1 if uppercase, 0 if lowercase
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
