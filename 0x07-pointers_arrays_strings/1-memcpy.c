d6786638cf2ebf5046808bb4b55293828b709a33
David Livingstone <livingst76@yahoo.com>
2023-08-28 20:02:23 +0100

    Change return variable in comment and code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,17 +5,16 @@
  * @src: memory area to be copied from.
  * @n: number of bytes to be copied.
  *
- * Return: pointer to copied memory area.
+ * Return: dest, pointer to copied memory area.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *ptr = dest;
 
 	for (i = 0; i < n; i++, dest++)
 	{
 		*dest = src[i];
 	}
 
-	return (ptr);
+	return (dest);
 }
