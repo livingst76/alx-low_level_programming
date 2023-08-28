f8b55b71c66c60979db2632e8b110d19537c8195
David Livingstone <livingst76@yahoo.com>
2023-08-28 19:51:23 +0100

    Implement custom function memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _memcpy - copy memory area.
+ *
+ * @dest: memory area to be copied to.
+ * @src: memory area to be copied from.
+ * @n: number of bytes to be copied.
+ *
+ * Return: pointer to copied memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *ptr = dest;
+
+	for (i = 0; i < n; i++, dest++)
+	{
+		*dest = src[i];
+	}
+
+	return (ptr);
+}
