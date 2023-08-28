03abfc4935b16948875d81648332e766b6b9b9e8
David Livingstone <livingst76@yahoo.com>
2023-08-29 00:53:44 +0100

    Update syntax

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,11 +14,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i, j, n;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i]; i++)
 	{
 		for (j = 0, n = i; haystack[n] == needle[j] &&
-				 needle[j] != '\0' && haystack[n] != '\0'; j++)
+				 needle[j] && haystack[n] != '\0'; j++)
+		{
 			n++;
+		}
 
 		if (needle[j] == '\0')
 		{
