17b4705f02215fbc5cc5b72efd7f893acffa47b1
David Livingstone <livingst76@yahoo.com>
2023-08-28 18:51:05 +0100

    Implemnent custom memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - fill memory with constant byte
+ *
+ * @s: string
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ *
+ * Return: s, pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
