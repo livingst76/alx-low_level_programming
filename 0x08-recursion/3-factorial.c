786e75634207bd093068177a875bfb9cbc4c1a1b
David Livingstone <livingst76@yahoo.com>
2023-08-30 17:23:03 +0100

    Create factorial calcuating function

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/**
+ * factorial - calculate factorial of number.
+ *
+ * @n: number.
+ *
+ * Return: factorial of number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
