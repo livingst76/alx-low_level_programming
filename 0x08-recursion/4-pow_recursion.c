8e715d19e8578241804fb8405f2f0e87e71ea861
David Livingstone <livingst76@yahoo.com>
2023-08-30 17:40:07 +0100

    Make integer indices calculation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _pow_recursion - calculate power of number raised to another number.
+ *
+ * @x: number.
+ * @y: power of x.
+ *
+ * Return: value of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
