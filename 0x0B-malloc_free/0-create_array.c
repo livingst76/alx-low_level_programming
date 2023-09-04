24c1e59a76c86cf2dd080d31f54edcc6c6cde0ff
David Livingstone <livingst76@yahoo.com>
2023-09-04 17:41:12 +0100

    Function allocates mem for array and initialises it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars; initialise it with a given char
+ *
+ * @size: size of array
+ * @c: given char
+ *
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *b;
+
+	if (!size)
+	{
+		return (NULL);
+	}
+
+	b = malloc(size * sizeof(char));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+
+	while (size)
+	{
+		size--;
+		b[size] = c;
+	}
+	return (b);
+}
