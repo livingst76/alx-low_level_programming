dd4bf9d5002a454fc1098d9984ab0c067f447beb
David Livingstone <livingst76@yahoo.com>
2023-09-07 20:55:16 +0100

    Allocate memory and check for mem errors

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocate memory using malloc.
+ *
+ * @b: size of memory to be allocated.
+ *
+ * Return: pointer to allocated memory, 98 if malloc fails.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (!ptr)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
