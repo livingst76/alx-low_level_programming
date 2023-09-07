7aa733f2330d5102f431817eeb9db90c3fab1240
David Livingstone <livingst76@yahoo.com>
2023-09-08 00:42:14 +0100

    Fix: Check 10,12

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	else
+	else if (ptr && new_size && old_size)
 	{
 		tmp = ptr;
 		ptr = malloc(new_size);
