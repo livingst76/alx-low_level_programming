4246675d51622105da6ec38eeccadd9c2116b3d0
David Livingstone <livingst76@yahoo.com>
2023-09-08 01:07:05 +0100

    Fix: Check 10,12

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,11 +42,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		}
 
-		
+		if (new_size > old_size)
+		{
 			new_size = old_size;
-		
+		}
 		new = memcpy(new, ptr, new_size);
 		free(ptr);
-		return (ptr);
+		return (new);
 	}
 }
