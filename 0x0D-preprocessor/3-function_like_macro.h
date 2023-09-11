87ab80e6d3081a849dab1263944390a14ae446d4
David Livingstone <livingst76@yahoo.com>
2023-09-11 14:56:29 +0100

    Fixed compilation error

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS(x)
-#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
-#endif /*ABS(x)*/
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (x < 0 ? ((x) * -1) : (x))
+
+#endif /*FUNCTION_LIKE_MACRO_H*/
