a426871154b0e6880962cb84c29bf07b2e3c81b4
David Livingstone <livingst76@yahoo.com>
2023-09-14 13:56:37 +0100

    Create test files for codes

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceihghhhnbvnjsdss", 'B', 3, NULL, "stSchool", NULL);
+    return (0);
+}
