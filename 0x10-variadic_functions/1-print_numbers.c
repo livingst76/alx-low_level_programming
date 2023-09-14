2a3029119c8dfa3f8782af907070d87080cc47e1
David Livingstone <livingst76@yahoo.com>
2023-09-14 14:44:26 +0100

    Print numbers passed to a variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers; terminate with a new line.
+ *
+ * @separator: string to be printed betweeen numbers (comma,...)
+ * @n: number of integers passed to the function.
+ *
+ * Return: nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (!separator)
+	{
+		return;
+	}
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", va_arg(ap, int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+
+	va_end(ap);
+}
