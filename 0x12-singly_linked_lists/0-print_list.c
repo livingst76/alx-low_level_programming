aba85d8794fd0c4473d704a30ce9b08741e4a06b
David Livingstone <livingst76@yahoo.com>
2023-09-21 20:22:58 +0100

    Print content of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a linked list.
+ *
+ * @h: linked list.
+ *
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	const list_t *head = h;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		i++;
+	}
+	h = head;
+
+	return (i);
+}
