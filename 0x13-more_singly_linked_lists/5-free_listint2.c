fcd917ee7f1685ffc5f64bc62d3950522939302d
David Livingstone <livingst76@yahoo.com>
2023-09-25 20:03:05 +0100

    Free memory of linked list using double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free allocated memory of linked list.
+ *
+ * @head: pointer address of linked list head.
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
