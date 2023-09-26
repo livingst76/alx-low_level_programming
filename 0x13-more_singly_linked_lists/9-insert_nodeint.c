c742da42f6d162c4e5a306d21ae642f0710c344a
David Livingstone <livingst76@yahoo.com>
2023-09-26 16:12:58 +0100

    Previous commit did not work

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,10 +17,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
+	{
 		return (NULL);
-
-	if (!head || !*head)
-		return (NULL);
+	}
 
 	tmp = *head;
 	while (i <= idx && *head)
