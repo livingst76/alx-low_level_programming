d16ef8d6ec11ab96ee31b093ad128bbf997da111
David Livingstone <livingst76@yahoo.com>
2023-10-01 23:02:14 +0100

    Free memory for a circular linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - free memory of linked list; handle loop cases.
+ *
+ * @h: linked list.
+ *
+ * Return: size of list that was free'd.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *current, *array[10000];
+	int is_loop = 0, i = 0, n = 0;
+
+	while (*h)
+	{
+		current = (*h)->next;
+		for (i = 0; i < n; i++)
+		{
+			if ((*h)->next == array[i])
+			{
+				free(*h);
+				*h = NULL;
+				is_loop = 1;
+				size += sizeof(*h);
+				break;
+			}
+		}
+
+		if (is_loop)
+		{
+			break;
+		}
+
+		size += sizeof(*h);
+		array[n] = *h;
+		free(*h);
+		*h = current;
+		n++;
+	}
+
+	/**h = NULL;*/
+	return (size);
+}
