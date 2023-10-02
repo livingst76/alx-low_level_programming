223110c89695cf8829ba9b874de086b065b70523
David Livingstone <livingst76@yahoo.com>
2023-10-02 22:54:29 +0100

    Write into file only if buffer is not empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,10 +28,14 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 
-		bytes_write = write(fd, text_content, strlen(text_content));
-		if (bytes_write == -1)
+		if (text_content)
 		{
-			return (-1);
+			bytes_write = write(fd, text_content, strlen(text_content));
+			if (bytes_write == -1)
+			{
+				return (-1);
+			}
+
 		}
 
 		bytes_close = close(fd);
