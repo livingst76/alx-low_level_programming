dde31a45a3b154e3341fdcfafefb954f14ed7e10
David Livingstone <livingst76@yahoo.com>
2023-10-03 20:07:15 +0100

    Do not be verbose in code when validating buffer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	if (text_content)
 	{
 		bytes_write = write(fd, text_content, strlen(text_content));
 		if (bytes_write == -1)
