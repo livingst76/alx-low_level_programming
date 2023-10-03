043ebec00ff7de89c4b0fd62e875d4b6eb61a0d7
David Livingstone <livingst76@yahoo.com>
2023-10-03 19:23:50 +0100

    Explicitly compare buffer to NULL character

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 
-		if (text_content)
+		if (text_content != NULL)
 		{
 			bytes_write = write(fd, text_content, strlen(text_content));
 			if (bytes_write == -1)
