2761e93b28076e1025a6f9630048de98ab5a35b0
David Livingstone <livingst76@yahoo.com>
2023-10-03 20:31:22 +0100

    Append text in buffer to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - append text at the end of file.
+ *
+ * @filename: name of file to be appended.
+ * @text_content: NULL terminated string to write to file.
+ *
+ * Return: 1 (success), -1 (failure).
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t bytes_write;
+	int fd = -1;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		bytes_write = write(fd, text_content, strlen(text_content));
+		if (bytes_write == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+
+}
