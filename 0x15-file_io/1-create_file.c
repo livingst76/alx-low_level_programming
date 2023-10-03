6f474d10b7e281b22970b18ad31969f6f9c3cdd8
David Livingstone <livingst76@yahoo.com>
2023-10-03 19:08:48 +0100

    Revert to previous file permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_TRUNC);
 	if (fd == -1)
 	{
-		fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR);
+		fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 		if (fd == -1)
 		{
 			return (-1);
