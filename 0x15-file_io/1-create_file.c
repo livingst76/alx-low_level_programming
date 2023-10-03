04cece45d51ea3118ee198a76972cf18a788e1f8
David Livingstone <livingst76@yahoo.com>
2023-10-03 18:41:27 +0100

    Add file permission parameter when truncating if file exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <string.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
  * create_file - create a file.
@@ -19,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_TRUNC);
+	fd = open(filename, O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
