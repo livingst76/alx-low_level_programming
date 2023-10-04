d25234f3a6949f68e3b43b4efc7f458f60a502f5
David Livingstone <livingst76@yahoo.com>
2023-10-04 21:14:54 +0100

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int validate(char *file_from);
+int _create_file(char *file_to, int fd1);
+void copy_file(char *buffer, char *file_from, char *file_to, int fd1, int fd2);
+void close_file(char *buffer, int fd);
 
 #endif /*MAIN_H*/
