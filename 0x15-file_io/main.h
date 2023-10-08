efd5627e43f9ff067da393a8776b9b029faf9272
David Livingstone <livingst76@yahoo.com>
2023-10-08 20:09:27 +0100

    Update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,16 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#if defined(__LP64__)
+#define Elfw(type) Elf64_ ## type
+#else
+#define Elfw(type) Elf32_ ## type
+#endif
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -12,5 +22,6 @@ int validate(char *file_from);
 int _create_file(char *file_to, int fd1);
 void copy_file(char *buffer, char *file_from, char *file_to, int fd1, int fd2);
 void close_file(char *buffer, int fd);
+int is_elf(int *fd, char *elf_file, Elfw(Ehdr) * header);
 
 #endif /*MAIN_H*/
