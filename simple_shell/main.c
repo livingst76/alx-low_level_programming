853e31151291bbc287fad3af9212828ef8f643e4
David Livingstone <livingst76@yahoo.com>
2023-10-10 21:40:07 +0100

    My first line of code

---
+++ simple_shell/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point for simple shell project.
+ *
+ * @argc: number of command line arguments.
+ * @argv: command line arguments.
+ *
+ * Return: 0 (success), matching error nunber on failure.
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+	printf("My Simple Shell Project\n");
+
+	return (0);
+}
