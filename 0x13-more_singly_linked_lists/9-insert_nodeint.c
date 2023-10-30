997e28b68c0fdfa3cd0d817a1275fb8eb3e8bac6
David Livingstone <livingst76@yahoo.com>
2023-10-30 14:04:33 +0100

    Explicitly check for null double pointer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
 	tmp = *head;
 	while ((i <= idx && *head) || (!*head && i == 0))
 	{
